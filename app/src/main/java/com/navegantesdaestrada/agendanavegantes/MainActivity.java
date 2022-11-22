23252b44cd2318c0b951e59bf2f4dd4658e0c549
SUPORTE <theo.ansi@gmail.com>
2022-11-22 16:28:15 -0300

    22/11/2022 - botão de Cadastro de usuário

--- app/src/main/java/com/navegantesdaestrada/agendanavegantes/MainActivity.java
+++ app/src/main/java/com/navegantesdaestrada/agendanavegantes/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         btnCadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), cadastro_usuario.class);
+                Intent intent = new Intent(getApplicationContext(), cadastroUsua.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/navegantesdaestrada/agendanavegantes/cadastroUsua.java
+++ app/src/main/java/com/navegantesdaestrada/agendanavegantes/cadastroUsua.java
@@ -4,34 +4,40 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.FirebaseDatabase;
 
-public class cadastro_usuario extends AppCompatActivity {
-    private FirebaseDatabase referencia = FirebaseDatabase.getInstance();
+public class cadastroUsua extends AppCompatActivity {
     private FirebaseAuth usuario = FirebaseAuth.getInstance();
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cadastro_usuario);
+        setContentView(R.layout.activity_cadastro_usua);
 
-        usuario.createUserWithEmailAndPassword("theo.ansi@gmail.com.br", "12345")
-                .addOnCompleteListener(cadastro_usuario.this, new OnCompleteListener<AuthResult>() {
+
+    }
+    //Cadastrar usuário
+    public void cadastrar (View view){
+        usuario.createUserWithEmailAndPassword("theooo@gmail.com", "123abc")
+                .addOnCompleteListener(cadastroUsua.this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            Toast.makeText(cadastro_usuario.this, "Cadastro efetuado comsucesso", Toast.LENGTH_LONG).show();
+                            Toast.makeText(cadastroUsua.this, "Cadastro efetuado comsucesso", Toast.LENGTH_LONG).show();
                         } else {
-                            Toast.makeText(cadastro_usuario.this, "Erro ao fazer o cadastro", Toast.LENGTH_LONG).show();
+                            Toast.makeText(cadastroUsua.this, "Erro ao fazer o cadastro", Toast.LENGTH_LONG).show();
                         }
+
                     }
-                }
-        );
+                });
+
     }
 }
