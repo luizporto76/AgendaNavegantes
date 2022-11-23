a5bd0f1ebf4abc0437b70059e822e4949c1b6a7e
theoa <theo.ansi@gmail.com>
2022-11-22 23:22:17 -0300

    22-11-2022 Cadastro de usuário finalizado. Resta tela de fazer login

--- app/src/main/java/com/navegantesdaestrada/agendanavegantes/MainActivity.java
+++ app/src/main/java/com/navegantesdaestrada/agendanavegantes/MainActivity.java
@@ -3,14 +3,19 @@ package com.navegantesdaestrada.agendanavegantes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class MainActivity extends AppCompatActivity {
     
-    private Button btnAgenda, btnCadastrar;
+    private Button btnAgenda, btnCadastrar, btnPublica;
+    private FirebaseAuth usuario = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +38,25 @@ public class MainActivity extends AppCompatActivity {
         btnCadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), cadastroUsua.class);
+                Intent intent = new Intent(getApplicationContext(), CadastroUsua.class);
                 startActivity(intent);
+            }
+        });
+
+        //Chama tela de cadastro de eventos
+        btnPublica = findViewById(R.id.buttonPublicar);
+        btnPublica.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(usuario.getCurrentUser() != null){
+                    Intent intent = new Intent(getApplicationContext(), CadastroEventos.class);
+                    startActivity(intent);
+                }else{
+                    Snackbar snackbar = Snackbar.make(view, "usuario nao logado", Snackbar.LENGTH_LONG );
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                }
             }
         });
     }
